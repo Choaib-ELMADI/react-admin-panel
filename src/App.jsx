d289c2c20344ea4793785a1193e61ab3a56cc6f2
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-05-01 13:34:37 +0100

    Set up all the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { Home, List, Login, Single, New } from './pages/index';
 import './App.css';
@@ -8,12 +9,33 @@ import './App.css';
 const App = () => {
   return (
     <div className='app'>
-      <h1>Dashboard</h1>
-      <Home />
-      <List />
-      <Login />
-      <Single />
-      <New />
+      <BrowserRouter>
+        <h1>Dashboard</h1>
+        <Routes>
+          <Route path='/'>
+
+            <Route index  element={ <Home /> } />
+
+            <Route path='login'  element={ <Login /> } />
+
+            <Route path='users'>
+              <Route index element={ <List /> } />
+              <Route path=':userId' element={ <Single /> } />
+              <Route path='new'    element={ <New /> } />
+            </Route>
+
+            <Route path='products'>
+              <Route index element={ <List /> } />
+              <Route path=':productId' element={ <Single /> } />
+              <Route path='new'    element={ <New /> } />
+            </Route>
+
+          </Route>
+
+          <Route path='*' element={ <Login /> } />
+          
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
